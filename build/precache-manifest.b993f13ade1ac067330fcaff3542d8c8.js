8321967d957dd991aa0f33874842059bf228acd6
Jhonny Martinez <jmartinez@ideabti.com>
2020-02-25 23:05:52 +1100

    Build files added

---
+++ build/precache-manifest.b993f13ade1ac067330fcaff3542d8c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bc217ad0901f3d23f1e6ee22d5d24c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31fc1f3ed44dc970c6b7",
+    "url": "/static/css/main.d64474ca.chunk.css"
+  },
+  {
+    "revision": "45683ffc3f3d65db8004",
+    "url": "/static/js/2.815d836f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.815d836f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fc1f3ed44dc970c6b7",
+    "url": "/static/js/main.f844c562.chunk.js"
+  },
+  {
+    "revision": "b769c41bdcb9f213bc8a",
+    "url": "/static/js/runtime-main.eb4913b6.js"
+  }
+]);
